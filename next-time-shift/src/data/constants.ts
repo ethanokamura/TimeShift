87c8a950ad2515553704f2b3cf083d1f6df70541
Ethan <ethanokamura3@gmail.com>
2025-04-09 20:11:22 -0700

    app's constant data

---
+++ next-time-shift/src/data/constants.ts
@@ -0,0 +1,12 @@
+
+export const title = "suprnova"
+export const description = "we make stars align"
+export const DAYS_OF_WEEK = [
+  "monday",
+  "tuesday",
+  "wednesday",
+  "thursday",
+  "friday",
+  "saturday",
+  "sunday",
+] as const;
