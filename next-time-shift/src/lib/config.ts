00511a8da65250ccad751f43b839248aaf061a98
Ethan <ethanokamura3@gmail.com>
2025-04-09 20:21:22 -0700

    directory cleanup

--- next-time-shift/src/lib/config.ts
+++
@@ -1,2 +0,0 @@
-export const title = "TimeShift"
-export const description = "A more efficient way to use your time."
--- next-time-shift/src/lib/supabase.ts
+++
@@ -1,8 +0,0 @@
-import { SupabaseAdapter } from "@auth/supabase-adapter";
- 
-// Create Supabase Adapter for AuthJS
-export const supabaseAdapter = SupabaseAdapter({
-  url: process.env.SUPABASE_URL!,
-  secret: process.env.SUPABASE_SERVICE_ROLE_KEY!,
-});
-
